3e2b403a6cc7dc5a5adaf0b483cee97155bcfd30
jimchristie <jimirings@gmail.com>
2018-05-03 21:56:34 +0000

    WIP: initial work on lazyloading images

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,31 @@
-// alert("hello, world");
+function lazyLoadImage(element, image){
+  console.log('element: ' + element);
+  console.log('image', image);
+  
+}
+
+function lazyLoadBackground(){
+  var elements = document.querySelectorAll('[data-background-image]');
+  var element;
+  var backgroundImage;
+  var backgroundPosition;
+  
+  for (var i=0; i < elements.length; i++){
+    element = elements[i];
+    backgroundImage = 'url(' + element.dataset.backgroundImage + ')';
+    element.style.backgroundImage = backgroundImage;
+    
+    if (element.dataset.backgroundPosition) {
+      backgroundPosition = element.dataset.backgroundPosition;
+      element.style.backgroundPosition = backgroundPosition;
+    }
+  }
+}
+
+function foo(message){
+  console.log(message);
+}
+
+window.onload = function(){
+  lazyLoadBackground();
+};
