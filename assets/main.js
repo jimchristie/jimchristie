474fb79652174311009a5422619f1917af91dd8d
jimchristie <jimirings@gmail.com>
2018-07-20 20:25:51 +0000

    Updated lazy loading to be more forgiving of screen sizes and load when scrolling. Various refactoring improvements.

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,177 @@
+/******************** Global vars *******************************************/
+
+var lastScrollTop = 0; // used to check scrolling direction
+
+
+
+
+/********************* functions that determine if elements are visible or nearly visible **********************/
+
+
 function hiddenByCss(element) {
   return element.offsetParent === null;
 }
 
+function windowHeight() {
+  return window.innerHeight || document.documentElement.clientHeight;
+}
+
+function windowWidth() {
+  
+}
+
 function inViewport(element) {
-  var rect = element.getBoundingClientRect();
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth
+  
+  element.position = element.getBoundingClientRect();
   
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight) && 
-      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-    ) || (
-      rect.bottom >= 0 &&
-      rect.right >= 0 &&
-      rect.bottom <= (window.inerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    element.position.top >= 0 &&
+    element.position.left >= 0 &&
+    element.position.top <= windowHeight && 
+    element.position.left <= windowWidth
+  ) || (
+    element.position.bottom >= 0 &&
+    element.position.right >= 0 &&
+    element.position.bottom <= windowHeight &&
+    element.position.right <= windowWidth
+  );
 }
 
 function isVisible(element){
   return inViewport(element) && !hiddenByCss(element);
 }
+
+function pixelMargin(percentage) {
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+  return windowHeight * (percentage / 100);
+}
+
+function isJustAboveViewport(element, margin) {
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  margin = (typeof margin === 'undefined') ? 10 : margin;
+  element.position = element.getBoundingClientRect();
+  
+  return (
+    element.position.bottom > -(pixelMargin(margin)) && 
+    element.position.bottom < 0
+  );
+}
+
+function isJustBelowViewport(element, margin) {
+  margin = (typeof margin === 'undefined') ? 10 : margin;
+  
+  var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  element.position = element.getBoundingClientRect();
+  
+  return (
+    element.position.top < windowHeight + pixelMargin(margin) && 
+    element.position.top > windowHeight
+  );
+  
+}
+
+function nearlyVisible(element, margin) {
+  return (isJustBelowViewport(element, margin) || isJustAboveViewport(element, margin)) && !hiddenByCss(element);
+}
+
+
+
+/********************************* determine scroll directions *************************************/
+
+
+/**
+ * uses global var lastScrollTop
+ */
+function scrollingDown() { 
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      return true;
+   } else {
+      return false;
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}
+
+/**
+ * uses global var lastScrollTop
+ */
+function scrollingUp() {
+  var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+  if (st < lastScrollTop){
+    return true;
+  } else {
+    return false;
+  }
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}
+
+function isComingIntoView(element, margin) {
+  margin = typeof margin === 'undefined' ? 50 : margin;
+  
+  return (
+    (scrollingUp() && isJustAboveViewport(element, margin) ) ||
+    (scrollingDown() && isJustBelowViewport(element, margin) )
+  );
+}
+
+/*************************** Lazy loading image functions *****************************************/
+
+var loadImage = function(element){
+  if (element.dataset && element.dataset.image){
+    element.setAttribute('src', element.dataset.image);
+    element.removeAttribute('data-image');
+  }
+};
+
+var lazyLoadImages = function(margin){
+  var elements= document.querySelectorAll('[data-image]');
+  
+  for (var i = 0; i < elements.length; i++){
+    // this runs in an IIFE to make the timeout work
+    // ref: http://borgs.cybrilla.com/tils/javascript-for-loop-with-delay-in-each-iteration-using-iife/
+    (function(i){
+      setTimeout(function(){
+        
+        if ( isVisible(elements[i]) || nearlyVisible(elements[i], margin) || isComingIntoView(elements[i])){
+          loadImage(elements[i]);
+        }
+      }, 1 * i);
+    })(i);
+  }
+};
+
+var setBackgroundPosition = function(element){
+  var backgroundPosition = element.dataset.backgroundPosition;
+  element.style.backgroundPosition = backgroundPosition;
+  element.removeAttribute("data-background-position");
+};
+
+var loadBackgroundImage = function(element){
+  var backgroundImage = 'url(' + element.dataset.backgroundImage + ')';
+  element.style.backgroundImage = backgroundImage;
+
+  if (element.dataset.backgroundPosition)
+    setBackgroundPosition(element);
+  
+      
+  element.removeAttribute("data-background-image");
+};
+
+var lazyLoadBackgroundImages = function(margin){
+  var elements = document.querySelectorAll('[data-background-image]');
+  
+  for (var i = 0; i < elements.length; i++){
+    if ( isVisible(elements[i]) || nearlyVisible(elements[i], margin) || isComingIntoView(elements[i]) ){
+      loadBackgroundImage(elements[i]);
+      
+    }
+  }
+};
+
+/******************** Functions that deal with external links *******************************/
 
 function linkIsExternal(element) {
   return (element.host !== window.location.host);
@@ -39,54 +190,6 @@ function getExternalLinks(){
   return externalLinks;
 }
 
-var loadImage = function(element){
-  if (element.dataset && element.dataset.image){
-    element.setAttribute('src', element.dataset.image);
-    element.removeAttribute('data-image');
-  }
-};
-
-var lazyLoadImages = function(){
-  var elements= document.querySelectorAll('[data-image]');
-  
-  for (var i = 0; i < elements.length; i++){
-    // this runs in an IIFE to make the timeout work
-    // ref: http://borgs.cybrilla.com/tils/javascript-for-loop-with-delay-in-each-iteration-using-iife/
-    (function(i){
-      setTimeout(function(){
-        if ( isVisible(elements[i]) ){
-          loadImage(elements[i]);
-        }
-      }, 1 * i);
-    })(i);
-  }
-};
-
-var loadBackgroundImage = function(element){
-  var backgroundImage = 'url(' + element.dataset.backgroundImage + ')';
-  element.style.backgroundImage = backgroundImage;
-};
-
-var setBackgroundPosition = function(element){
-  var backgroundPosition = element.dataset.backgroundPosition;
-  element.style.backgroundPosition = backgroundPosition;
-};
-
-var lazyLoadBackgroundImages = function(){
-  var elements = document.querySelectorAll('[data-background-image]');
-  
-  for (var i = 0; i < elements.length; i++){
-    if ( isVisible(elements[i]) ){
-      loadBackgroundImage(elements[i]);
-
-      if (elements[i].dataset.backgroundPosition)
-        setBackgroundPosition(elements[i]);
-      
-      elements[i].removeAttribute("data-background-image");
-    }
-  }
-};
-
 var addTargetBlankToExternalLinks = function() {
   var externalLinks = getExternalLinks();
   
@@ -96,8 +199,12 @@ var addTargetBlankToExternalLinks = function() {
   for (var i = 0; i < externalLinks.length; i++ ) {
     externalLinks[i].setAttribute("target", "_blank");
   }
-  
 };
+
+
+/*
+ ********************** init and events *************************************************
+ */
 
 window.onload = function(){
   lazyLoadBackgroundImages();
@@ -111,6 +218,6 @@ window.onscroll = function(){
 };
 
 window.onresize = function(){
-  lazyLoadBackgroundImages;
+  lazyLoadBackgroundImages();
   lazyLoadImages();
 };
